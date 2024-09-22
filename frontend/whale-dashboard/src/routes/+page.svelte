37d0bf5db47c8fc6021c185c9b2acc3fa8dd8180
Taylorgtyler <taylorgtyler@gmail.com>
2024-09-22 10:20:38 -0500

    styled navbar + added bitcoin logo svg

--- frontend/whale-dashboard/src/routes/+page.svelte
+++ frontend/whale-dashboard/src/routes/+page.svelte
@@ -1,21 +1,16 @@
 <script>
 	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Bitcoin Whale Tracker" />
 </svelte:head>
 
 <section>
 	<h1>
 		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
+
 		</span>
 
 		to your new<br />SvelteKit app
@@ -47,13 +42,5 @@
 		width: 100%;
 		height: 0;
 		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
