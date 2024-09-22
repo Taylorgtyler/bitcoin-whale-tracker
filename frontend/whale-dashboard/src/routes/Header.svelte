a034831bdc7f60074561ba1a85be8824c962eca5
Taylorgtyler <taylorgtyler@gmail.com>
2024-09-22 11:02:26 -0500

    Added glossary page to nav

--- frontend/whale-dashboard/src/routes/Header.svelte
+++ frontend/whale-dashboard/src/routes/Header.svelte
@@ -16,8 +16,8 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/glossary' ? 'page' : undefined}>
+				<a href="/glossary">Glossary</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
